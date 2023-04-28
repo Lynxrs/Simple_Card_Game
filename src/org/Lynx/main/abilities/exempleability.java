8d56cf684505b9f357586d4473256cf0cedb0e70
Lynx <101434584+Lynxr0se@users.noreply.github.com>
2023-04-28 02:12:52 +0200

    still not inished but usable

--- src/org/Lynx/main/abilities/exempleability.java
+++ src/org/Lynx/main/abilities/exempleability.java
@@ -3,9 +3,37 @@ package org.Lynx.main.abilities;
 public class exempleability {
     private String name;
     private Boolean Ismagic;
-    private int cost;
+    private int manacost;
     private int dmgphys;
     private int dmgmag;
+
+    public exempleability(String name, Boolean ismagic, int manacost, int dmgphys, int dmgmag) {
+        this.name = name;
+        this.Ismagic = ismagic;
+        this.manacost = manacost;
+        this.dmgphys = dmgphys;
+        this.dmgmag = dmgmag;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Boolean getIsmagic() {
+        return Ismagic;
+    }
+
+    public int getManacost() {
+        return manacost;
+    }
+
+    public int getDmgphys() {
+        return dmgphys;
+    }
+
+    public int getDmgmag() {
+        return dmgmag;
+    }
 
 
 }
