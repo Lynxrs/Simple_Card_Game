eecbd8e34586466b0c3be578d4cd3bf24eed4fbe
Lynx <101434584+Lynxr0se@users.noreply.github.com>
2023-06-16 15:16:19 +0200

    -added new effect class
    Took 50 seconds

---
+++ src/org/Lynx/main/effects/effects.java
@@ -0,0 +1,41 @@
+package org.Lynx.main.effects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class effects {
+    private String name;
+    private int ID;
+    private int time;
+    private int power;
+    private List<effects> registeredeffects = new ArrayList<>();
+
+    public effects(String name, int time, int power) {
+        this.name = name;
+        this.time = time;
+        this.power = power;
+    }
+
+
+
+    public void  registerEffect(effects effect){
+        generateeffectID(registeredeffects, effect);
+        registeredeffects.add(effect);
+    }
+
+    public static void generateeffectID(List<effects> effectlist, effects effect){
+        effect.setID(effectlist.size());
+    }
+
+
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    };
+
+
+}
