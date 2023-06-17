bf226884dc1405325e2920b1d8cbd8953db62081
Lynx <101434584+Lynxr0se@users.noreply.github.com>
2023-06-17 03:16:19 +0200

    -added use fonction who usd to be the BaseAttack Function -adda few getter and setter for effects system
    Took 1 minute

--- src/org/Lynx/main/abilities/exempleability.java
+++ src/org/Lynx/main/abilities/exempleability.java
@@ -1,5 +1,15 @@
 package org.Lynx.main.abilities;
 
+import org.Lynx.main.Main;
+import org.Lynx.main.cards.Player;
+import org.Lynx.main.cards.exemplecard;
+import org.Lynx.main.effects.effects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.Lynx.main.Main.print;
+
 public class exempleability {
     private String name;
     private Boolean Ismagic;
@@ -7,14 +17,20 @@ public class exempleability {
     private int dmgphys;
     private int dmgmag;
     private Boolean HasEffect;
+    private String effectname;
+    private List<effects> effectlist = new ArrayList<>();
 
-    public exempleability(String name, Boolean ismagic, int manacost, int dmgphys, int dmgmag, Boolean hasEffect) {
+    public exempleability(String name, Boolean ismagic, int manacost, int dmgphys, int dmgmag, Boolean hasEffect, String effectID) {
         this.name = name;
         this.Ismagic = ismagic;
         this.manacost = manacost;
         this.dmgphys = dmgphys;
         this.dmgmag = dmgmag;
-        HasEffect = hasEffect;
+        this.HasEffect = hasEffect;
+        if(this.getHasEffect()){
+            this.effectname = effectID;
+        }
+
     }
 
     public String getName() {
@@ -24,7 +40,26 @@ public class exempleability {
     public Boolean getIsmagic() {
         return Ismagic;
     }
+    public void use(exemplecard caster, exemplecard target, Player pcaster, Player ptarget){
+        if (caster == target && pcaster == ptarget){
+            print("vous ne pouvez vous infliger des dégats");
+        }
+        if (pcaster.getSelectedabl().getIsmagic()){
+            exempleability abl = pcaster.getSelectedabl();
+            caster.magdmg(target, abl.getManacost());
+            if(abl.getHasEffect()){
+                target.getEffectlist().add(abl.getEffectname());
+            }
 
+        } else if (!pcaster.getSelectedabl().getIsmagic()) {
+            exempleability abl = pcaster.getSelectedabl();
+            caster.physdmg(target);
+            if(abl.getHasEffect()){
+                target.getEffectlist().add(Main.registeredabilities.get(Main.chooseability).getEffectname());
+            }
+        }
+
+    }
     public int getManacost() {
         return manacost;
     }
@@ -38,4 +73,15 @@ public class exempleability {
     }
 
 
+    public Boolean getHasEffect() {
+        return HasEffect;
+    }
+
+    public void setHasEffect(Boolean hasEffect) {
+        HasEffect = hasEffect;
+    }
+
+    public String getEffectname() {
+        return effectname;
+    }
 }
