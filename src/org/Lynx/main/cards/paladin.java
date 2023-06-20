f7953036cab9243b8c5d264ecbf3c427055fa3e2
Lynx <101434584+Lynxr0se@users.noreply.github.com>
2023-06-21 00:36:02 +0200

    -made paladin class for the 1st special card currentrly working on it

---
+++ src/org/Lynx/main/cards/paladin.java
@@ -0,0 +1,9 @@
+package org.Lynx.main.cards;
+
+public class paladin extends exemplecard {
+    public paladin(String nom, int pv, int maxpv, int dmgphys, int dmgmag, int defphys, int defmag, int maxmana, int mana, boolean baismagic) {
+        super(nom, pv, maxpv, dmgphys, dmgmag, defphys, defmag, maxmana, mana, baismagic);
+    }
+
+
+}
